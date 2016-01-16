446332605016f3456769bb958ef1af0dc98dd396
Russell Richardson <Russell@KronosAD.com>
2016-01-15 20:27:23 -0600

    Implemented OAuth in login activity

--- app/src/main/java/com/kronosad/android/raft/LoginActivity.java
+++ app/src/main/java/com/kronosad/android/raft/LoginActivity.java
@@ -1,30 +1,59 @@
 package com.kronosad.android.raft;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String OAUTH_URL = "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=ecb4694ef92bcb1991d71a5ebd68af050ffe83bbfedebe811cdb1d425e914a91&redirect_uri=raft://authorize/&response_type=code";
+    private static final String EXTRA_CUSTOM_TABS_SESSION = "android.support.customtabs.extra.SESSION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if(getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            retrieveTokenAndPersist();
+        }
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
+    }
+
+    @OnClick(R.id.activity_login_btn)
+    public void onLoginBtnClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(OAUTH_URL));
+
+        Bundle extras = new Bundle();
+        extras.putBinder(EXTRA_CUSTOM_TABS_SESSION,
+                null);
+        intent.putExtras(extras);
+
+        startActivity(intent);
+
     }
 
     @Override
@@ -40,5 +69,22 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void retrieveTokenAndPersist() {
+        Intent intent = getIntent();
+        String token = intent.getData().getQueryParameter("code");
+        Log.d("Raft", "Retrieved Token: " + token);
+
+        SharedPreferences prefs = getSharedPreferences("Authentication", MODE_PRIVATE);
+        prefs.edit().putString("token", token).apply();
+
+        openDropletActivity();
+    }
+
+    private void openDropletActivity() {
+
+
+        finish(); // Prevent user from going back to this activity.
     }
 }
